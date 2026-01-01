ffe12e009e0c1d88121070631176839072ffa909
nephi@blaviken-wsl <nephi.norton@gmail.com>
2025-12-31 23:13:35 -0700

    clear button + some styles

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -8,6 +8,12 @@ type Square = {
 
 const squares: Ref<Square[]> = ref([])
 
+function clearSquares() {
+  for (const square of squares.value) {
+    square.filled = false
+  }
+}
+
 for (let i = 0; i < 30; i++) {
   squares.value.push({ id: i, filled: i % 3 == 0 })
 }
@@ -18,9 +24,11 @@ for (let i = 0; i < 30; i++) {
     <div v-for="square in squares" class="square" :key="square.id" :class="square.filled ? 'filled-square' : ''"
       @click.prevent="square.filled = !square.filled"></div>
   </div>
+
+  <button class="green" @click="clearSquares">Clear</button>
 </template>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
 #board-box {
   display: grid;
   grid-template-columns: auto auto auto auto auto;
@@ -32,9 +40,26 @@ for (let i = 0; i < 30; i++) {
   min-width: 64px;
   min-height: 64px;
   margin: 4px;
+  transition: 0.08s ease-out;
 }
 
 .filled-square {
   background-color: var(--green-highlight);
+}
+
+button {
+  margin: 4px;
+  padding: 4px 11px;
+  background-color: var(--green-lowlight);
+  border: 1px solid var(--color-border);
+  border-radius: 4px;
+  font-size: large;
+}
+
+button:hover {
+  color: white;
+  cursor: pointer;
+  background-color: var(--green-highlight);
+  transition: 0.1s ease-out;
 }
 </style>
